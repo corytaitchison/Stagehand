079fb5eca35c95c157dbd57e2e9d1ef40d4d83b2
Cory Aitchison <corytaitchison@gmail.com>
2021-11-30 21:28:48 +1100

    Add basic svelte site.

---
+++ site/site_progress.md
@@ -0,0 +1,23 @@
+# Stagehand Website
+
+> Website built using Svelte
+
+## 2021-11-30 8pm
+
+Getting started with Svelt
+- Installed node and nvm using https://www.jurnalanas.com/node-js-mac-m1/ 
+- Installing degit and using it to create a template https://svelte.dev/blog/the-easiest-way-to-get-started
+
+```zsh
+degit sveltejs/template stagehand-site
+cd stagehand-site
+# to use TypeScript run:
+node scripts/setupTypeScript.js
+
+npm install
+````
+
+Then to run the development server
+```zsh
+npm run dev
+```
---
+++ site/stagehand-site/src/App.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { fade} from "svelte/transition";
+	export let name: string;
+	let show: Boolean = false;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	{#if show}
+	<p transition:fade>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to
+		learn how to build Svelte apps.
+	</p>
+	{/if}
+	<button on:click="{() => {show = !show;}}">Click me!</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
